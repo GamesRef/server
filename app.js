9b2478b8fed625ccd2a523a527d6d51de2e3aff4
andresudi <y.andresudi@gmail.com>
2018-09-07 16:04:11 +0700

    done quote

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var youtubeRouter = require('./routes/youtubeRoute');
 var dotaRouter = require('./routes/dota')
+var quotesRouter = require('./routes/quotes');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/youtube', youtubeRouter);
 app.use('/dota', dotaRouter)
+app.use('/quotes', quotesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,4 @@ const wikiController = require('./../controllers/wikiController.js')
 router.get('/unsplash/collection', unsplashController.list)
 router.post('/wiki/search', wikiController.searchWiki)
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-
 module.exports = router;
