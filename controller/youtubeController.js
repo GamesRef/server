a053ad4415957aeeed11163c5f5cf12f219b6e31
Renaldy Pratama <renaldy.pratama19@gmail.com>
2018-09-07 14:14:27 +0700

    minor fix search query

--- controller/youtubeController.js
+++ controller/youtubeController.js
@@ -3,11 +3,9 @@ const axios = require('axios');
 module.exports = {
   search: function(req,res) {   
     var q = req.body.q
-    var year = (new Date).getFullYear();
-    var dateFormat = `${year}-01-01T00:00:00-08:00`
     axios({
       method:'get',
-      url:`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&order=relevance&publishedAfter=${dateFormat}&type=video&key=${process.env.YOUTUBE_KEY}`
+      url:`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&order=relevance&maxResults=6&regionCode=US&type=video&key=${process.env.YOUTUBE_KEY}`
     })
       .then(function(response) {
         res.status(200).json(response.data)
@@ -19,12 +17,12 @@ module.exports = {
   nextPage: function(req,res) {
     var q = req.body.q
     var nextPageToken = req.body.nextPageToken
-    var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&order=relevance&publishedAfter=${dateFormat}&type=video&key=${process.env.YOUTUBE_KEY}`
     axios({
       method:'get',
-      url:`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&order=relevance&publishedAfter=${dateFormat}&type=video&key=${process.env.YOUTUBE_KEY}`
+      url:`https://www.googleapis.com/youtube/v3/search?pageToken=${nextPageToken}&part=snippet&q=${q}&order=relevance&maxResults=6&regionCode=US&type=video&key=${process.env.YOUTUBE_KEY}`
     })
       .then(function(response) {
+        console.log(response.data)
         res.status(200).json(response.data)
       })
       .catch(err => {
